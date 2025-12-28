e0a3e785f9d1f0db801ddc39603608ce7fda89ad
victorhappe <viha1000@stud.ek.dk>
2025-12-28 16:17:15 +0100

    Forside1

---
+++ menu.js
@@ -0,0 +1,9 @@
+const burger = document.querySelector(".burger");
+const nav = document.querySelector("nav");
+
+burger.addEventListener("click", burgerClick);
+
+function burgerClick() {
+  burger.classList.toggle("active");
+  nav.classList.toggle("active");
+}
